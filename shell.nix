5d0254ab74a3c59420cdfa25be5b2840500388bb
Daniel Barter <danielbarter@gmail.com>
2021-12-29 10:28:28 -0800

    jax nix template

---
+++ shell.nix
@@ -0,0 +1,19 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    python3
+    python3Packages.jax
+    (python3Packages.jaxlib.override { cudaSupport = true; })
+    nvtop
+  ];
+
+  LD_LIBRARY_PATH = with pkgs; builtins.concatStringsSep ":" [
+    # cuda shared libraries
+    "${cudatoolkit_11_2}/lib"
+    "${cudatoolkit_11_2.lib}/lib"
+
+    # nvidia driver shared libs
+    "${pkgs.linuxPackages.nvidia_x11}/lib"
+  ];
+}
