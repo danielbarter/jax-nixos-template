133076b688f51f3d9a6cd5721bfc4e1e60395944
Daniel Barter <danielbarter@gmail.com>
2022-01-03 15:50:35 -0800

    ..

--- geodesic.py
+++ geodesic.py
@@ -3,9 +3,9 @@ import jax.numpy as jnp
 import numpy as np
 
 @jax.jit
-def action(displacement):
+def action(displacement, dt=1):
     squares = displacement * displacement
-    return jnp.sqrt(squares.sum())
+    return squares.sum() * dt
 
 @jax.jit
 def lagrangian(
@@ -30,23 +30,36 @@ def lagrangian(
     return accumulator
 
 
-
-def update(points, start, end):
-    grad = jax.grad(lagrangian)(points, start, end)
-    factor = grad.max()
-    return points -  factor * grad
+@jax.jit
+def update(points, start, end, factor=0.1):
+    return points -  factor * jax.grad(lagrangian)(points, start, end)
 
 
 
-dimension = 300
+def run(initial_points, start, end, num_steps):
+    points = initial_points
+    lagrangian_vals = []
 
-points = jax.random.normal(jax.random.PRNGKey(42), shape=(100,dimension))
+    for step in range(num_steps):
+        points = update(points, start, end)
+        val = lagrangian(points, start, end)
+        lagrangian_vals.append(val)
+        if step % 1000 == 0:
+            print("step:      ", step)
+            print("lagrangian:", val)
+
+    return points, lagrangian_vals
+
+
+
+##################################################################
+
+
+num_points = 100
+dimension = 3000
+
+points = jax.random.normal(jax.random.PRNGKey(42), shape=(num_points,dimension))
 start = jnp.full(dimension, 0.0)
 end = jnp.full(dimension, 1.0)
 
-while(True):
-    points = update(points, start, end)
-    error = (jnp.abs(points[7][1:] - points[7][:-1])).sum()
-    print(error)
-    if error < 1.0:
-        break
+result, lagrangian_vals = run(points, start, end, 50000)
