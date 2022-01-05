ce205360a3d1cfff7540eed069b4147a07871b92
Daniel Barter <danielbarter@gmail.com>
2022-01-05 09:39:48 -0800

    ..

--- test.py
+++ test.py
@@ -20,29 +20,41 @@ def straight_line_test():
 
 
 def saddle_test_2d():
-    num_points = 100
+    num_points = 200
     dimension = 2
-    num_steps = 10000
+    num_steps = 20000
 
     points = jax.random.normal(jax.random.PRNGKey(42), shape=(num_points,dimension))
-    start = jnp.array([0.0,-1.22])
-    end = jnp.array([0.0, 1.22])
+    start = jnp.array([0.5,-1.2])
+    end = jnp.array([0.5, 1.2])
 
     result, lagrangian_vals = run(test_saddle, points, start, end, num_steps)
-    fig, axs = plt.subplots(2, 1, figsize=(5,10), gridspec_kw={'height_ratios':[1,1]})
-    axs[1].set_ylim([0.0,20.0])
-    axs[1].plot(lagrangian_vals)
+    fig, axs = plt.subplots(3, 1, figsize=(5,15), gridspec_kw={'height_ratios':[1,1, 1]})
+    axs[2].set_ylim([0.0,3.0])
+    axs[2].plot(lagrangian_vals)
+    axs[2].set_title("lagrangian values")
     x_vals = np.arange(-2.0,2.0,0.01)
     y_vals = np.arange(-2.0,2.0,0.01)
     z_vals = np.zeros((x_vals.shape[0], y_vals.shape[0]))
+    # obviously, there is some better way of doing this
     for i in range(x_vals.shape[0]):
         for j in range(y_vals.shape[0]):
             z_vals[j,i] = test_saddle(np.array([x_vals[i],y_vals[j]]))
 
+    axs[0].set_title("contor plot")
     axs[0].contour(x_vals, y_vals, z_vals)
     axs[0].scatter(result[:,0], result[:,1])
 
+
+    grad_norms = []
+    for i in range(result.shape[0]):
+        grad_norms.append(jnp.square(jnp.square(jax.grad(test_saddle)(result[i])).sum()))
+
+    axs[1].set_title("gradient magnitude")
+    axs[1].plot(grad_norms)
+
     fig.savefig("/tmp/2d_saddle_test.pdf")
+
 
 # straight_line_test()
 saddle_test_2d()
